9f37c13dff1c83994fd1939f74eae96f9767a0fa
dustin pfister <dustin.pfister@gmail.com>
2019-08-15 14:43:31 -0400

    new comments for echo_pipe

--- demos/linux-echo/echo_pipe.js
+++ demos/linux-echo/echo_pipe.js
@@ -1,6 +1,9 @@
-
+// do something with data that is being
+// piped in from the standard input from a 
+// command like linux echo
 process.stdin.on('data', (data) => {
-    
     console.log(data.toString('hex'));
-    
 });
+
+// $ echo "hello" | node echo_pipe
+// 68656c6c6f0a
