67271554fc394520403592d4a67ee9a8d593a514
dustinpfister <dustin.pfister@gmail.com>
2020-10-09 12:23:36 -0400

    clean list posts example

--- demos/linux-pipe/list_posts.js
+++ demos/linux-pipe/list_posts.js
@@ -1,8 +1,26 @@
 // list the URIS for all markdown files in the current working path or given path
-let exec = require('child_process').exec;
+let exec = require('child_process').exec,
+path = require('path');
 
-let find = exec('find *.js');
+let dir_root = process.argv[2] || process.cwd(),
+dir_find = path.join(dir_root, '*.md');
 
+// start find command
+let find = exec('find ' + dir_find),
+lines = false;
 find.stdout.on('data', (data) => {
-  process.stdout.write(data.toString() + '\n');
+    let URI = data.toString().split('\n');
+    URI = URI.map((uri)=>{
+        return '\'' + uri + '\'';
+    });
+    if(URI[URI.length - 1] === '\'\''){
+        URI.pop();
+    }
+    lines = true;
+    process.stdout.write('[' + URI);
+});
+find.on('exit', (data) => {
+    if(lines){
+        process.stdout.write(']\n');
+    }
 });
