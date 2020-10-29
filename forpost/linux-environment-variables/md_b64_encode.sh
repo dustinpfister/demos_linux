104779b33f703c5368991b33717e67fb391d6a7d
dustinpfister <dustin.pfister@gmail.com>
2020-10-29 15:10:37 -0400

    md_b64_encode.sh example

--- forpost/linux-environment-variables/md_b64_encode.sh
+++ forpost/linux-environment-variables/md_b64_encode.sh
@@ -3,5 +3,6 @@ for i in $( ls *.md ); do
     file=$i
     fileBaseName=$( echo -n "$file"  | cut -d '.' -f 1 )
     fileEXT=$( echo -n $file | cut -d '.' -f 2 )
-    cat $file | base64 "done ${fileBaseName}.b64"
+    cat $file | base64 > "${fileBaseName}.${fileEXT}.b64"
 done
+echo 'done'
