bcbffcec9cf1f5de73684d0fb4a67f55f368e1d4
dustinpfister <dustin.pfister@gmail.com>
2020-10-29 15:02:47 -0400

    linux-environment-variables folder

---
+++ forpost/linux-environment-variables/loop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in $( ls *.sh ); do
+    file=$i
+    fileBaseName=$( echo -n "$file"  | cut -d '.' -f 1 )
+    fileEXT=$( echo -n $file | cut -d '.' -f 2 )
+    echo "done ${fileBaseName}"
+done
