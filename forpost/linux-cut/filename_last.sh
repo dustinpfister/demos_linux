43b4838ce6d60e9cabb47750e3a7a1c5150af22d
dustinpfister <dustin.pfister@gmail.com>
2020-11-19 14:00:35 -0500

    removed empty line

--- forpost/linux-cut/filename_last.sh
+++ forpost/linux-cut/filename_last.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 filepath="/home/pi/foo.bar/.fonts/filename.txt.b64"
 fcount=$( echo -n $filepath | grep -o "\/" | wc -w )
 filename=$( echo -n $filepath | cut -d '/' -f $(( $fcount + 1)) )
