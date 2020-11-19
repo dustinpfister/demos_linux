1512d6cf8e2872d6527623465e3a4b52cac2ec80
dustinpfister <dustin.pfister@gmail.com>
2020-11-19 13:56:30 -0500

    filename last example

---
+++ forpost/linux-cut/filename_last.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+filepath="/home/pi/foo.bar/.fonts/filename.txt.b64"
+fcount=$( echo -n $filepath | grep -o "\/" | wc -w )
+filename=$( echo -n $filepath | cut -d '/' -f $(( $fcount + 1)) )
+basename=$( echo -n $filename | cut -d '.' -f 1 )
+echo $basename
