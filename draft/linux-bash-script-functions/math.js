d9bf33e5f6ba9434f8ef434e636c1d3c17d880bb
dustinpfister <dustin.pfister@gmail.com>
2020-11-20 15:00:01 -0500

    comments

--- draft/linux-bash-script-functions/math.js
+++ draft/linux-bash-script-functions/math.js
@@ -1,22 +1,23 @@
 #!/usr/bin/node
+// parse options
 var prop = process.argv[2],
 len = process.argv.length;
 args = process.argv.slice(3, len);
-
+// log
 var log = function(num){
     process.stdout.write(String(num) + '\n');
 };
 if(prop){
    var MathProp = Math[prop];
-
+   // a Math function
    if(typeof MathProp === 'function'){
        log(MathProp.apply(null, args));
    }
-
+   // a Math propery
    if(typeof MathProp === 'number'){
        log(MathProp);
    }
-
+   // some of my own
    if(typeof MathProp === 'undefined'){
       // multiply two numbers
       if(prop === 'multi'){
