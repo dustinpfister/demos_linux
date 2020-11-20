7afb506a0006c4814f8474c4671ce6b752d5918f
dustinpfister <dustin.pfister@gmail.com>
2020-11-20 15:18:45 -0500

    function argumnets

--- draft/linux-bash-script-functions/func_argu.sh
+++ draft/linux-bash-script-functions/func_argu.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# a function has its own set of positioned arguments
 func_add(){
     echo $(( $1 + $2 ))
 }
 
-func_add "3" "2"
+# using shell argumnets too
+let "a = 0"
+let "b = 0"
+if [ ! -z $1 ]; then
+   let "a = $1"
+fi
+if [ ! -z $2 ]; then
+   let "b = $2"
+fi
+# can call a function with argumnets like this
+func_add $a $b
