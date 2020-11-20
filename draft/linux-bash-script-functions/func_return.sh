d6b8a6f739eafe964085bc3bf0977a812c0cd9f7
dustinpfister <dustin.pfister@gmail.com>
2020-11-20 13:42:42 -0500

    example_math strated

--- draft/linux-bash-script-functions/func_return.sh
+++ draft/linux-bash-script-functions/func_return.sh
@@ -8,16 +8,4 @@ math_pow(){
     echo $n
 }
 
-## math_sqrt n
-math_sqrt(){
-    echo $(awk "BEGIN {printf \"%.30f\n\", sqrt($1)}")
-}
-
-# math_dist x1 y1 x2 y2
-math_dist(){
-    local a=$(($1 ** $2))
-    echo $n
-}
-
 echo $(math_pow 2 4)
-echo $(math_sqrt 25)
