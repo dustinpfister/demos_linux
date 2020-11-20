7e89afa2f58cae6e68d7468f388c2cf978421d79
dustinpfister <dustin.pfister@gmail.com>
2020-11-20 14:58:29 -0500

    added some of my own

--- draft/linux-bash-script-functions/math.js
+++ draft/linux-bash-script-functions/math.js
@@ -3,15 +3,35 @@ var prop = process.argv[2],
 len = process.argv.length;
 args = process.argv.slice(3, len);
 
+var log = function(num){
+    process.stdout.write(String(num) + '\n');
+};
 if(prop){
    var MathProp = Math[prop];
 
    if(typeof MathProp === 'function'){
-       var result = String(MathProp.apply(null, args));
-       process.stdout.write(result + '\n');
+       log(MathProp.apply(null, args));
    }
 
    if(typeof MathProp === 'number'){
-       process.stdout.write(String(MathProp) + '\n');
+       log(MathProp);
    }
+
+   if(typeof MathProp === 'undefined'){
+      // multiply two numbers
+      if(prop === 'multi'){
+          log(args[0] * args[1]);
+      }
+      // distance between two points
+      if(prop === 'dist'){
+          var a = Math.pow(args[0] - args[2], 2)
+          b = Math.pow(args[1] - args[3], 2);
+          log(Math.sqrt( a + b));
+      }
+      // eval an expression
+      if(prop === 'eval'){
+          log(eval(args[0]));
+      }
+   }
+
 }
