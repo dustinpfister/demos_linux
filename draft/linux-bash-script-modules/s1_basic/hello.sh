39478e018a9f7f2de84c139d52aae63c382bdd07
dustinpfister <dustin.pfister@gmail.com>
2020-12-03 13:07:42 -0500

    renamed file

--- draft/linux-bash-script-modules/s1_basic/hello.sh
+++
@@ -1,4 +0,0 @@
-mess=$(./mod_hello.sh hw)
-sayText=$(./mod_hello.sh say Dustin)
-
-echo "${mess}, and ${sayText}"
---
+++ draft/linux-bash-script-modules/s1_basic/use_lib.sh
@@ -0,0 +1,4 @@
+mess=$(./mod_lib.sh hw)
+sayText=$(./mod_lib.sh say Dustin)
+
+echo "${mess}, and ${sayText}"
