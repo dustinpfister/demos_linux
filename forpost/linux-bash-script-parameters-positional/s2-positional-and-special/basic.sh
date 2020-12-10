541950f7388cb059df8c6ce6eae0d2abfa0015a2
dustinpfister <dustin.pfister@gmail.com>
2020-12-10 14:55:17 -0500

    s3 folder for positionals

---
+++ forpost/linux-bash-script-parameters-positional/s2-positional-and-special/basic.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "number of arguments: $#"
+echo "arguments: $@"
---
+++ forpost/linux-bash-script-parameters-positional/s3-positional-and-bash/bash-string.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# just calling the script
+./all.sh 1 2
+
+# calling bash first with the -c option
+bash all.sh 1 2
