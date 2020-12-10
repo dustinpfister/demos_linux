2d978f328572752af9e4fda7f4b56c051d7069d5
dustinpfister <dustin.pfister@gmail.com>
2020-12-10 14:58:01 -0500

    bash example for s3

--- forpost/linux-bash-script-parameters-positional/s3-positional-and-bash/bash-string.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# just calling the script
-./all.sh 1 2
-
-# calling bash first with the -c option
-bash all.sh 1 2
---
+++ forpost/linux-bash-script-parameters-positional/s3-positional-and-bash/bash.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo ""
+
+# just calling the script
+./all.sh 1 2
+
+echo ""
+
+# calling bash first with some options
+bash -v all.sh 1 2
