ef11aff9960dd0ee0f49f258f81a6450a5ca5df0
dustinpfister <dustin.pfister@gmail.com>
2021-02-11 11:25:57 -0500

    ls example

---
+++ forpost/linux-cd/s1_basics/ls.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /bin
+ls -l b*
+
+# -rwxr-xr-x 1 root root 925124 Apr 18  2019 bash
+# -rwxr-xr-x 3 root root  26100 Jul 10  2019 bunzip2
+# -rwxr-xr-x 1 root root 743212 Apr  1  2019 busybox
+# -rwxr-xr-x 3 root root  26100 Jul 10  2019 bzcat
+# lrwxrwxrwx 1 root root      6 Jul 10  2019 bzcmp -> bzdiff
+# -rwxr-xr-x 1 root root   2227 Jul 10  2019 bzdiff
+# lrwxrwxrwx 1 root root      6 Jul 10  2019 bzegrep -> bzgrep
+# -rwxr-xr-x 1 root root   4877 Jun 24  2019 bzexe
+# lrwxrwxrwx 1 root root      6 Jul 10  2019 bzfgrep -> bzgrep
+# -rwxr-xr-x 1 root root   3641 Jul 10  2019 bzgrep
+# -rwxr-xr-x 3 root root  26100 Jul 10  2019 bzip2
+# -rwxr-xr-x 1 root root   9632 Jul 10  2019 bzip2recover
+# lrwxrwxrwx 1 root root      6 Jul 10  2019 bzless -> bzmore
+# -rwxr-xr-x 1 root root   1297 Jul 10  2019 bzmore
