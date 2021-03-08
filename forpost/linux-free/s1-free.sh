093ac0b943b76164374d4682a18ea4b15c32aefb
dustinpfister <dustin.pfister@gmail.com>
2021-03-08 12:17:58 -0500

    linux-free forpost folder started

---
+++ forpost/linux-free/s1-free.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+free
+#              total        used        free      shared  buff/cache   available
+#Mem:         946392      374608       54864       45020      516920      474404
+#Swap:       1048572       90880      957692
+
+free -h
+#              total        used        free      shared  buff/cache   available
+#Mem:          924Mi       377Mi        47Mi        43Mi       499Mi       451Mi
+#Swap:         1.0Gi        90Mi       933Mi
