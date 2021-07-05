70783e2aafd2e54f3e58dde3140583437292fbf4
dustinpfister <dustin.pfister@gmail.com>
2021-07-05 15:56:50 -0400

    commnets

--- forpost/linux-rm/s3_rd.sh
+++ forpost/linux-rm/s3_rd.sh
@@ -6,3 +6,11 @@ echo "foo" > "./foo/foo.txt"
 echo "bar" > "./foo//bar/bar1.txt"
 echo "bar" > "./foo//bar/bar2.txt"
 echo "foobar" > "./foo//baz/foobar/foobar.txt"
+
+ls foo
+#bar  baz  foo.txt
+
+rm -rd foo
+
+ls foo
+#ls: cannot access 'foo': No such file or directory
