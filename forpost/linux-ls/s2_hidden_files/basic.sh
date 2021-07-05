b1637096707b7fe8f0ca0ae0ba7c9eb7b1bcbbb4
dustinpfister <dustin.pfister@gmail.com>
2021-07-05 15:44:15 -0400

    remove files

--- forpost/linux-ls/s2_hidden_files/basic.sh
+++ forpost/linux-ls/s2_hidden_files/basic.sh
@@ -2,3 +2,6 @@ echo "hello" > hello.txt
 echo "I hide things" > .hide.txt
 ls *.txt
 #hello.txt
+
+rm hello.txt
+rm .hide.txt
--- forpost/linux-ls/s2_hidden_files/list_all.sh
+++ forpost/linux-ls/s2_hidden_files/list_all.sh
@@ -2,3 +2,6 @@ echo "hello" > hello.txt
 echo "I hide things" > .hide.txt
 ls -a 
 # .  ..  hello.txt  .hide.txt
+
+rm hello.txt
+rm .hide.txt
