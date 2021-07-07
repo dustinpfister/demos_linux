23a53b111e48f8b022db87c668a7a423d0c747b2
dustinpfister <dustin.pfister@gmail.com>
2021-07-07 14:16:42 -0400

    commnets

--- draft/linux-uname/s3-other-options.sh
+++ draft/linux-uname/s3-other-options.sh
@@ -1,8 +1,16 @@
 uname -s
+#Linux
 uname -n
+#raspberrypi
 uname -r
+#5.10.17-v7l+
 uname -v
+##1421 SMP Thu May 27 14:00:13 BST 2021
 uname -m
+#armv7l
 uname -p
+#unknown
 uname -i
+#unknown
 uname -o
+#GNU/Linux
