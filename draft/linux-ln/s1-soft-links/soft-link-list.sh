6ff46277d0925c2b6c6da7f63de09121b7671ee2
dustinpfister <dustin.pfister@gmail.com>
2021-09-25 10:55:29 -0400

    clean up

--- draft/linux-ln/s1-soft-links/soft-link-list.sh
+++ draft/linux-ln/s1-soft-links/soft-link-list.sh
@@ -4,3 +4,7 @@ echo "hello world" > foo.txt
 ln -s foo.txt fooLink
 ls -la | grep "\->"
 # lrwxrwxrwx 1 pi pi    7 Sep 25 10:50 fooLink -> foo.txt
+
+# clean up
+rm fooLink
+rm foo.txt
--- draft/linux-ln/s1-soft-links/soft-link.sh
+++ draft/linux-ln/s1-soft-links/soft-link.sh
@@ -4,3 +4,7 @@ echo "hello world" > foo.txt
 ln -s foo.txt fooLink
 cat fooLink
 #hello world
+
+# clean up
+rm fooLink
+rm foo.txt
