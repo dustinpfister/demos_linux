e0d795f72bcf8730e11ce9ea3da2f57278b68a76
dustinpfister <dustin.pfister@gmail.com>
2021-10-09 11:59:04 -0400

    using exit

---
+++ forpost/linux-test/s3-custom-test/echo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./test-has-md.sh; echo $?
+#1
+./test-has-md.sh posts; echo $?
+#0
--- forpost/linux-test/s3-custom-test/test-has-md.sh
+++ forpost/linux-test/s3-custom-test/test-has-md.sh
@@ -2,5 +2,4 @@
 MDCOUNT=$(ls $1 | grep ".md$" | wc -l)
 STATUS=1;
 if [ $MDCOUNT -gt 0 ]; then STATUS=0; fi
-
-echo $STATUS;
+exit $STATUS;
