ec0235df25858bda3b01db701c3e38c4f4861f0e
dustinpfister <dustin.pfister@gmail.com>
2021-10-09 14:50:18 -0400

    looking good

---
+++ forpost/linux-test/s3-custom-test/test-c/test-has-md.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <regex.h>        
+
+regex_t regex;
+int reti;
+int mdcount = 0;
+
+int main(int argc, char *argv[])
+{
+    DIR *folder;
+    struct dirent *entry;
+    int files = 0;
+    char *dirpath =  ".";
+    if(argc > 1){
+        dirpath = argv[1];
+    }
+
+    folder = opendir(dirpath);
+    if(folder == NULL)
+    {
+        perror("Unable to read directory");
+        return(1);
+    }
+
+    while( (entry=readdir(folder)) )
+    {
+        files++;
+        reti = regcomp(&regex, "\\.md$", 0);
+        if(reti){
+            // failed to compile a regex
+            return 2;
+        }
+        reti = regexec(&regex, entry->d_name, 0, NULL, 0);
+        if(!reti){
+            mdcount++;
+            //printf("%s\n", entry->d_name);
+        }
+        regfree(&regex);
+    }
+    
+    closedir(folder);
+    // if mdcount is greater than 0 then return exit with code 0
+    if(mdcount > 0){
+        printf("%i\n", mdcount);
+        return 0;
+    }
+    // else exit with code 1
+    return 1;
+}
