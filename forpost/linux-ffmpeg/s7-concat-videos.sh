3678735af9542bba88c75fb7174acc667dd42e06
dustinpfister <dustin.pfister@gmail.com>
2022-03-05 11:35:02 -0500

    s7 concat example looking good

--- forpost/linux-ffmpeg/s7-concat-videos.sh
+++ forpost/linux-ffmpeg/s7-concat-videos.sh
@@ -1,6 +1,13 @@
 
-ffmpeg -framerate 30 -i ./frames/frame-%04d.png -pix_fmt yuv420p video1.mp4
-ffmpeg -i ./video1.mp4 -fps 15 -pix_fmt yuv420p video2.mp4
+ffmpeg -y -framerate 30 -i ./frames/frame-%04d.png -pix_fmt yuv420p video1.mp4
 
-# ls video[0-9]*.mp4 | awk '{ printf "file \x27%s\x27\n", $0 }' > videos.txt
-# ls ffmpeg -f concat -i videos.txt -c copy video.mp4
+# fps filter for setting lower fps, but with same length
+# ffmpeg -i ./video1.mp4 -vf fps=fps=10 video2.mp4
+
+# setpts filter can be used to increase length
+ffmpeg -y -i ./video1.mp4 -vf setpts=2.5*PTS video2.mp4
+
+# concat videos
+ls video[0-9]*.mp4 | awk '{ printf "file \x27%s\x27\n", $0 }' > videos.txt
+cat ./videos.txt
+ffmpeg -f concat -i videos.txt -c copy video-concat.mp4
