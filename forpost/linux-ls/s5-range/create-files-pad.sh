82151d4e7f446cf251c609d3c9b374ac4ac347c2
dustinpfister <dustin.pfister@gmail.com>
2022-03-05 12:49:12 -0500

    range working with single digit files that are not paded

--- forpost/linux-ls/s5-range/create-files-pad.sh
+++ forpost/linux-ls/s5-range/create-files-pad.sh
@@ -1,8 +1,8 @@
 c=0
 while [ $c -le 10 ]
 do
-  #d=$(printf "%03d" $c)
-  fn="file${c}.txt" 
+  d=$(printf "%03d" $c)
+  fn="file${d}.txt" 
   echo $fn
   echo "This is file ${fn}" > $fn
   c=$(( $c + 1 ))
