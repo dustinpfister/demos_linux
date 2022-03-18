e59afcf8c4ba1be4e71ca5f5553624e5afc8e1fd
dustinpfister <dustin.pfister@gmail.com>
2022-03-18 13:53:40 -0400

    copying over background image

--- draft/linux-bash-script-example-raspberry-pi-os-setup/parts/1-bg.sh
+++ draft/linux-bash-script-example-raspberry-pi-os-setup/parts/1-bg.sh
@@ -4,10 +4,13 @@
 # * make this image at ~/Pictures/rpi-bg.png the background image
 #
 
+# get path to source image for setup type
 setup_type=${1:-work};
 dir_bg=$(realpath ../bg);
 path_source_image="${dir_bg}/bg_${setup_type}.png"
 
-echo -e "\n"
-echo $path_source_image
-echo -e "\n"
+# echoing what is going on for this script
+echo "setting background for "${setup_type}" setup"
+
+# copy the source image to the home folder
+cp "${path_source_image}" ~/Pictures/rpi-bg.png
