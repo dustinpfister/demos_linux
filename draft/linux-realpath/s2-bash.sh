811a12c079d8ad99ebd3263f403dcd0df5de3bd4
dustinpfister <dustin.pfister@gmail.com>
2022-03-18 11:27:25 -0400

    do not need to store pwd

--- draft/linux-realpath/s2-bash.sh
+++ draft/linux-realpath/s2-bash.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# storing current working dir
-dir_pwd=$(pwd);
-
 # cd to user lib locale
 cd /usr/lib/locale;
 
@@ -14,5 +11,3 @@ echo "dirname: $( dirname . )";      # .
 echo "realpath: $( realpath . )";    # /usr/lib/locale
 echo -e "**********\n";
 
-# return to pwd
-cd $dir_pwd;
