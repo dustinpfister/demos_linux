413ee2a2556276543e29fc40395ea9ba4c218d7a
dustinpfister <dustin.pfister@gmail.com>
2022-03-18 13:45:06 -0400

    getting relavent info for bg part

--- draft/linux-bash-script-example-raspberry-pi-os-setup/parts/1-bg.sh
+++ draft/linux-bash-script-example-raspberry-pi-os-setup/parts/1-bg.sh
@@ -3,3 +3,11 @@
 # * copy over a background image from this project folder to ~/Pictures/rpi-bg.png
 # * make this image at ~/Pictures/rpi-bg.png the background image
 #
+
+setup_type=${1:-work};
+dir_bg=$(realpath ../bg);
+path_source_image="${dir_bg}/bg_${setup_type}.png"
+
+echo -e "\n"
+echo $path_source_image
+echo -e "\n"
