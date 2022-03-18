2b24856cc5f688445677ccd75f19fc7bfd524480
dustinpfister <dustin.pfister@gmail.com>
2022-03-18 13:38:52 -0400

    parts folder

---
+++ draft/linux-bash-script-example-raspberry-pi-os-setup/parts/1-bg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# 1-bg.sh - set background image
+# * copy over a background image from this project folder to ~/Pictures/rpi-bg.png
+# * make this image at ~/Pictures/rpi-bg.png the background image
+#
