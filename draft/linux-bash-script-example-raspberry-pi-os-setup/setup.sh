4783f4daf4958cf683926e01033ad4945e126aea
dustinpfister <dustin.pfister@gmail.com>
2022-03-18 10:06:48 -0400

    setup.sh file

---
+++ draft/linux-bash-script-example-raspberry-pi-os-setup/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# What kind of setup? options are work, experiment, and play
+setup_type=${1:-work};
+dir_script=$(dirname $0 | xargs realpath);
+dir_home=$(echo $HOME);
+
+# Copy bg images
+echo "running setup for new raspberry pi image that will be used for ${setup_type}"
+echo "script dir ${dir_script}"
+echo "home path for current user ${dir_home}"
