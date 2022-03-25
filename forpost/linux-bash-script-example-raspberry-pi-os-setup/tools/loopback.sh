b1f2a7f25913301c5b2d853bc10e2251f868ada4
dustinpfister <dustin.pfister@gmail.com>
2022-03-25 12:11:57 -0400

    loopback starting to work

--- forpost/linux-bash-script-example-raspberry-pi-os-setup/tools/loopback.sh
+++ forpost/linux-bash-script-example-raspberry-pi-os-setup/tools/loopback.sh
@@ -3,6 +3,27 @@
 # exit with status 0 when file name is found else exit with 1
 # set a $DIR_[file basename] shell variable to the path in which the file is found
 
-DIR_START=${1:-/};
+DIR_START=${1:-~};
+folders=($(echo ${DIR_START} | tr "/" " "));
+len=${#folders[@]}
 
-echo $DIR_START;
+
+# start at DIR_START
+cd ${DIR_START};
+
+
+i=$len;
+while [ $i -gt 0 ]; do
+    i=$[$i-1];
+    echo "${i} ) ${folders[$i]}";
+    echo $(pwd);
+    echo "";
+    cd ..;
+done;
+
+
+#for i in ${folders[@]}; do
+#    fn="$i"
+#    echo -ne "$i\n";
+#        
+#done;
