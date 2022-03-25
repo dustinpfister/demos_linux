65602f2cacd81f88d041bcc7c3c4cd2c9c02690d
dustinpfister <dustin.pfister@gmail.com>
2022-03-25 12:52:03 -0400

    bashrc.txt and start of 2-bashrc part script

---
+++ forpost/linux-bash-script-example-raspberry-pi-os-setup/parts/2-bashrc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 2-bashrc.sh - check out the sich with .bashrc
+#    * use the given source file path for a backrc file to set at home
+#    * check for a .bashrc_backup
+#        * if there is one do nothing
+#        * if there is no .bashrc_backup check for a .bashrc file
+#            * if there is a .bashrc create a .bashrc_backup from that file
+#            * if there is no .bashrc file create a .bashrc_backup that is the same as the source file
+
+PATH_SOURCE=$(realpath ${1:-../bashrc.txt});
+
+echo $PATH_SOURCE;
