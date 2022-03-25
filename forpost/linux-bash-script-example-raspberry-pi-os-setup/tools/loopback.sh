2ab0e30256c67f50a356cf677ee905dfa3897bb1
dustinpfister <dustin.pfister@gmail.com>
2022-03-25 12:29:58 -0400

    loopback script working okay

--- forpost/linux-bash-script-example-raspberry-pi-os-setup/tools/loopback.sh
+++ forpost/linux-bash-script-example-raspberry-pi-os-setup/tools/loopback.sh
@@ -4,6 +4,8 @@
 # set a $DIR_[file basename] shell variable to the path in which the file is found
 
 DIR_START=${1:-~};
+FN=${2:-rpisetup.txt};
+DIR_FN="";
 folders=($(echo ${DIR_START} | tr "/" " "));
 len=${#folders[@]}
 
@@ -11,15 +13,30 @@ len=${#folders[@]}
 # start at DIR_START
 cd ${DIR_START};
 
-
+# loop back and try to find the file
 i=$len;
 while [ $i -gt 0 ]; do
     i=$[$i-1];
-    echo "${i} ) ${folders[$i]}";
-    echo $(pwd);
-    echo "";
+    #echo "${i} ) ${folders[$i]}";
+    #echo $(pwd);
+
+    ls ${FN} &> /dev/null;
+    if [ $? -eq 0 ]; then
+       #echo "WE FOUND ONE"
+       DIR_FN=$(pwd);
+       #cat ${FN}
+       #echo ""
+       break;
+    fi;
+    #echo "";
     cd ..;
 done;
+
+echo ""
+if [ "${DIR_FN}" != "" ]; then
+   echo "FOUND ONE"
+   echo "DIR_FN = ${DIR_FN}"
+fi;
 
 
 #for i in ${folders[@]}; do
