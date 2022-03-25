0b1eeeddec0073ecdb52d8937cbc30864119119a
dustinpfister <dustin.pfister@gmail.com>
2022-03-25 13:07:29 -0400

    comment

--- forpost/linux-bash-script-example-raspberry-pi-os-setup/parts/2-bashrc.sh
+++ forpost/linux-bash-script-example-raspberry-pi-os-setup/parts/2-bashrc.sh
@@ -6,6 +6,7 @@
 #        * if there is no .bashrc_backup check for a .bashrc file
 #            * if there is a .bashrc create a .bashrc_backup from that file
 #            * if there is no .bashrc file create a .bashrc_backup that is the same as the source file
+#    * write a new .bashrc file at home
 
 PATH_SOURCE=$(realpath ${1:-../bashrc.txt});
 
