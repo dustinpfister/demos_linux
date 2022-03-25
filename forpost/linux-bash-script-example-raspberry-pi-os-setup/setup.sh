f7e95bed303403c904126dc23662d384e1a5ff6b
dustinpfister <dustin.pfister@gmail.com>
2022-03-25 13:16:53 -0400

    updated setup.sh

--- forpost/linux-bash-script-example-raspberry-pi-os-setup/setup.sh
+++ forpost/linux-bash-script-example-raspberry-pi-os-setup/setup.sh
@@ -5,10 +5,17 @@ setup_type=${1:-work};
 dir_script=$(dirname $0 | xargs realpath);
 dir_home=$(echo $HOME);
 
-# Copy bg images
 echo "running setup for new raspberry pi image that will be used for ${setup_type}"
 echo "script dir ${dir_script}"
 echo "home path for current user ${dir_home}"
+echo -e "\n"
 
-# background
+# part1 - Copy bg images
+echo "Starting part1 - copy over background images, and set background"
 ./parts/1-bg.sh ${setup_type}
+echo -e "\n"
+
+# part2 - bashrc file
+echo "Starting part2 - backup/write new .bashrc file at home"
+./parts/2-bashrc.sh ./bashrc.txt
+echo -e "\n"
