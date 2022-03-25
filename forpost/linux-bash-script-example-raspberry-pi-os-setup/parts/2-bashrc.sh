bb836d62610c499c46f22640536ceb660c6c634d
dustinpfister <dustin.pfister@gmail.com>
2022-03-25 13:04:48 -0400

    will create backup from source file

--- forpost/linux-bash-script-example-raspberry-pi-os-setup/parts/2-bashrc.sh
+++ forpost/linux-bash-script-example-raspberry-pi-os-setup/parts/2-bashrc.sh
@@ -10,3 +10,21 @@
 PATH_SOURCE=$(realpath ${1:-../bashrc.txt});
 
 echo -e "Uisng source file for .bashrc found at: ${PATH_SOURCE} \n";
+
+# cd to home
+cd ~;
+
+# check for .bashrc_backup file
+if [ ! -f ".bashrc_backup" ]; then
+   echo "NO .bachrc_backup file found"
+   # check for a .bachrc file, if there is one create a backup
+   if [ -f ".bashrc" ]; then
+       echo ".bashrc file found, creating .bashrc_backup from that";
+       cat .bashrc > .bashrc_backup;
+   fi
+   # no .bashrc!? then create one from source file
+   if [ ! -f ".bashrc" ]; then
+       echo "NO .bashrc file found! Creating .bashrc_backup from source file.";
+       cat ${PATH_SOURCE} > .bashrc_backup;
+   fi
+fi
