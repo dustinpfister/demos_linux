fc170bf801ee7ee97000bd543463d33c6486cfaa
dustinpfister <dustin.pfister@gmail.com>
2023-06-16 20:39:31 -0400

    moving the wave count up and down over time

--- forpost/linux-aplay/s6-1-nodejs-livepipe/live_highlow.js
+++ forpost/linux-aplay/s6-1-nodejs-livepipe/live_highlow.js
@@ -10,14 +10,18 @@ const writeSample = (buff, i_sample = 0, count_sample = 8000, wave_count = 1, am
 // LOOP
 //-------- ----------
 const buff = Buffer.alloc(1);
+const frame_count = 100;
 let i_sample = 0;
+let i_frame = 0;
 const count_sample = 8000;
 let to_high = false;
 let last_time = new Date();
 const loop = () => {
     const t = setTimeout(loop, to_high ? 5000: 990);
+    const a_frame = i_frame / frame_count;
+    const a_wavecount = Math.sin( Math.PI * a_frame )
     while( i_sample < count_sample){
-        writeSample(buff, i_sample, count_sample, 10, 0.25);
+        writeSample(buff, i_sample, count_sample, Math.floor(50 + 250 * a_wavecount), 0.4);
         to_high = !process.stdout.write(buff);
         if(to_high){
             break;
@@ -25,6 +29,8 @@ const loop = () => {
         i_sample += 1;
     }
     i_sample %= count_sample;
+    i_frame += 1;
+    i_frame %= frame_count;
 };
 // drain event
 process.stdout.on('drain', () => {
